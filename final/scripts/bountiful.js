2a56c14d579a3ee8637ba0cfea86f1dce16c14ef
derekearl <ear17004@byui.edu>
2023-04-05 23:26:52 -0600

    shouldbeit

--- final/scripts/bountiful.js
+++ final/scripts/bountiful.js
@@ -8,37 +8,27 @@ const x = document.querySelector('#hamburger');
 x.onclick = toggleMenu;
 
 
-const apiURL = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/92008?unitGroup=us&key=57NALRV6F75DVP8VRY5NQWN5P&contentType=json';
-const getWeather = async () => {
-    const response = await fetch(apiURL);
-    const data = await response.json();
-    console.log(data);
+const weatherAPI = 'https://api.openweathermap.org/data/3.0/onecall?lat=33.15846&lon=-117.343&exclude=minutely,hourly,alerts&units=imperial&appid=b1159721bd65dac769dcd4f88420a53a'
+fetch(weatherAPI)
+    .then(res => res.json())
+    .then(data => {
+        const currentTemp = data.current.temp
+        const currentDescription = data.current.weather[0].description
+        const currentHumidity = data.current.humidity
+        const currentIcon = data.current.weather[0].icon
 
-    let t = data.currentConditions.temp;
-    document.querySelector('#degree').textContent = t.toFixed(1);
+        document.querySelector('.description').innerHTML = `${currentDescription}`
+        document.getElementById('weather-icon').src = `http://openweathermap.org/img/wn/${currentIcon}@2x.png`
+        document.querySelector('.temperature').innerHTML = `${currentTemp} °F`
+        document.querySelector('.humidity').innerHTML = `Humidity: ${currentHumidity}%`
+        document.querySelector('.forecast').innerHTML = `
+            <u><h3>3 Day Forecast (Day)</h3></u>
+            Day 1: ${data.daily[0].temp.day}°F <br>
+            Day 2: ${data.daily[1].temp.day}°F <br>
+            Day 3: ${data.daily[2].temp.day}°F <br>
+        `
+    })
 
-    let image = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/main/SVG/1st%20Set%20-%20Color/${data.currentConditions.icon}.svg`;
-    document.querySelector('#speed').textContent = data.currentConditions.windspeed;
-    document.querySelector('#weather-forecast').textContent =  data.currentConditions.conditions;
-    document.querySelector('#weather-icon').src = image;
-    document.querySelector('#weather-icon').alt= data.currentConditions.conditions + ' icon';
-
-    // Windchill Info Here
-    let degree = document.querySelector("#degree").textContent;
-    console.log(degree);
-    let speed = document.querySelector("#speed").textContent;
-    console.log(speed);
-
-    let chill = Math.round((35.74 + (0.6215 * degree))-(35.75 * Math.pow(speed,0.16)) + (0.4275*degree*Math.pow(speed,0.16)));
-
-    if (degree <= 50 && speed >= 3) {
-        document.querySelector("#chill").textContent = chill;
-    };
-
-    
-}
-
-getWeather();
 
 const date = new Date();
 const year = date.getFullYear();
